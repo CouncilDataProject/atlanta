710f035b2fe5ae350c5e70333944cae24e8e202f
evamaxfield <evamaxfieldbrown@gmail.com>
2022-10-05 18:59:14 +0000

    Deploying to gh-pages from @ CouncilDataProject/atlanta@ed2d5babc35e5cee8f1ad62363aebefb64309a78 🚀

---
+++ static/js/main.e2b387f3.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpcdp-atlanta"]=this["webpackJsonpcdp-atlanta"]||[]).push([[0],{204:function(t,e){},90:function(t,e,n){"use strict";n.r(e);n(0);var i=n(26),a=n.n(i),o=n(65),c=(n(218),n(57));a.a.render(Object(c.jsx)("div",{children:Object(c.jsx)(o.AppConfigProvider,{appConfig:{firebaseConfig:{options:{projectId:"cdp-atlanta-37e7dd70"},settings:{}},municipality:{name:"Atlanta",timeZone:"America/New_York",footerLinksSections:[]}},children:Object(c.jsx)(o.App,{})})}),document.getElementById("root"))}},[[90,1,2]]]);
+//# sourceMappingURL=main.e2b387f3.chunk.js.map
